7f9507853b758643579b7d91a203b21129e0499e
EdWard680 <ezeward4@gmail.com>
2015-01-18 15:22:21 -0500

    Fixes Loggable Objects to properly manage data

--- src/Loggable.cpp
+++ src/Loggable.cpp
@@ -19,6 +19,7 @@ const int NumericLog::logCurrent()
 {
 	out.write((char *)buf.data(), sizeof(double)*buf.size());
 	buf.clear();
+	out.flush();  // Ensures output stream is writing to file
 	return 0;
 }
 
@@ -32,14 +33,24 @@ const int BooleanLog::Log()
 
 	buf.back() |= fn()<<(curPos++);
 
-	if(buf.size() == flushBytes)
+	if(buf.size() == flushBytes && curPos == 7)
 		return logCurrent();
 	return 0;
 }
 
 const int BooleanLog::logCurrent()
 {
+	char tmp;
+	if(curPos != 7)
+	{
+		tmp = buf.back();  // Ensures 0s are not written to frames which did not occur
+		buf.pop_back();
+	}
 	out.write(buf.data(), buf.size());
+	curPos = 0;
 	buf.clear();
+	out.flush();
+	if(curPos != 7)
+		buf.push_back(tmp);  // Pushes back known frames
 	return 0;
 }
