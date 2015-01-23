b558026e9c374d26d973c4f86adfc51444161045
EdWard680 <ezeward4@gmail.com>
2015-01-23 08:56:45 -0500

    Adds config file

---
+++ src/Config.h
@@ -0,0 +1,26 @@
+#ifndef _CONFIG_H_
+#define _CONFIG_H_
+
+
+#define STATE_OUT_FILE_NAME "states.txt"
+#define LOG_HOME "/home/lvuser/log"
+
+#define DEFAULT_BUFFER_FRAMES 20
+
+#define FRAME_TEXT_WIDTH 5
+
+
+class LEVEL_t {
+public:
+	enum {EMER=0, ALERT, CRIT, ERR, WARN, NOTICE, INFO, DEBUG};
+	static const char* text[];
+};
+
+class SUBSYSTEM {
+public:
+	enum {TEST = 0};
+	static const char* text[];
+};
+
+
+#endif /*_CONFIG_H_*/
