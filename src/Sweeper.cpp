4e7dc6391e860fea9d72fc63d2450260ba9ef74c
Elijah Williams <elazertwist@gmail.com>
2015-02-15 20:32:54 -0500

    Updated Sweeper Code, still having problems

--- src/Sweeper.cpp
+++ src/Sweeper.cpp
@@ -9,11 +9,9 @@
 #include "WPILib.h"
 
 
-Sweeper::Sweeper(uint8_t m_talon)
+Sweeper::Sweeper(uint32_t m_talon)
 {
 	m_t = new CANTalon(m_talon);
-	m_currentState = SWEEPER_UP;
-	m_targetState = SWEEPER_UP;
 	m_needFree = true;
 	return;
 }
@@ -21,8 +19,6 @@ Sweeper::Sweeper(uint8_t m_talon)
 Sweeper::Sweeper(CANTalon &m_talon)
 {
 	m_t = &m_talon;
-	m_currentState = SWEEPER_UP;
-	m_targetState = SWEEPER_UP;
 	m_needFree = false;
 	return;
 }
@@ -30,65 +26,25 @@ Sweeper::Sweeper(CANTalon &m_talon)
 Sweeper::Sweeper(CANTalon *m_talon)
 {
 	m_t = m_talon;
-	m_currentState = SWEEPER_UP;
-	m_targetState = SWEEPER_UP;
 	m_needFree = false;
 	return;
 }
 
-Sweeper::sweeper_t
-Sweeper::setPosition(sweeper_t p) // either RAISES or LOWERS Sweeper based on Input value
+
+bool
+goForward(float motorSpeed)
 {
-	m_targetState = p;
-	switch(m_targetState) {
-		case SWEEPER_UP:
-			raiseSweeper();
-			break;
-		case SWEEPER_DOWN:
-			lowerSweeper();
-			break;
-		default:
-			break;
-	}
-	return m_targetState;
+     m_t->Set(motorSpeed);
 }
 
-Sweeper::sweeper_t
-Sweeper::getPosition()   //returns current state of Sweeper (SWEEPER_UP or SWEEPER_DOWN)
+bool
+goReverse(float motorSpeed)
 {
-	if (m_t->Get()==0)  //If GET from TALON object returns 0, then it is UP
-		m_currentState = SWEEPER_UP;
-	else
-		m_currentState = SWEEPER_DOWN;
-
-	return m_currentState;
+     m_t->Set(motorSpeed);
 }
 
-void
-Sweeper::lowerSweeper()
+bool
+Stop()
 {
-	//Figure out the current and Target Sweeper positions, see if movement is needed
-	m_currentState = getPosition();
-	setPosition(m_targetState);
-
-	//If currentPosition != targetPosition then move
-	if (m_currentState != m_targetState)
-			setPosition (m_targetState);
-	return;
+     talonMotor->Set(0);
 }
-
-void
-Sweeper::raiseSweeper()
-{
-//Figure out the current and Target Sweeper positions, see if movement is needed
-	m_currentState = Sweeper::getPosition();
-	setPosition(m_targetState);
-
-
-	//If currentPosition != targetPosition then move
-	if (m_currentState != m_targetState)
-			setPosition (m_targetState);
-	return;
-}
-
-
--- src/Sweeper.h
+++ src/Sweeper.h
@@ -13,33 +13,25 @@
 
 class Talon;
 class CANTalon;
-class DigitalInput;
-class Joystick;
-
 
 class Sweeper {
 
-	Sweeper(uint8_t m_talon);
+	Sweeper(uint32_t m_talon);
 	Sweeper(CANTalon &m_talon);
 	Sweeper(CANTalon *m_talon);
 
 	enum sweeper_t { SWEEPER_UP, SWEEPER_DOWN, NUM_STATES };
 
 
-	sweeper_t setPosition(sweeper_t p);
-	sweeper_t getPosition();
-	void lowerSweeper();
-	void raiseSweeper();
+	bool  goForward(float motorSpeed);
+	bool  goReverse(float motorSpeed);
+	bool  Stop();
 
 private:
 		CANTalon *m_t;
 		CANTalon m_motor;
-	    Joystick m_stick;
 		bool m_needFree;
-		DigitalInput *m_safety;
 
-		sweeper_t m_currentState;
-		sweeper_t m_targetState;
 
 };
 
