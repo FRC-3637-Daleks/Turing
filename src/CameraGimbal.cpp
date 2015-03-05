50d110011b94add727fa2b5275428f9121c0d76f
Elvin Kong <ekong1@hcrhs.org>
2015-03-05 15:47:22 -0500

    Added States to CameraGimbal

--- src/CameraGimbal.cpp
+++ src/CameraGimbal.cpp
@@ -2,6 +2,16 @@
 #include "stdio.h"
 #include "WPILib.h"
 
+const float CameraGimbal::xStates[] = {
+		[CameraGimbal::FRONT] = 0.0,
+		[CameraGimbal::BACK] = 1.0
+};
+
+const float CameraGimbal::yStates[] = {
+		[CameraGimbal::DOWN] = 0.0,
+		[CameraGimbal::HORIZONTAL] = 0.5
+};
+
 CameraGimbal::CameraGimbal(int servoIDX, int servoIDY,
 		float homeX, float homeY): m_servoX(servoIDX), m_servoY(servoIDY)
 {
@@ -30,3 +40,29 @@ void CameraGimbal::setHomePosition()
 	m_servoX.Set(m_homeX);
 	m_servoY.Set(m_homeY);
 }
+
+void CameraGimbal::setState(xPos_t x, yPos_t y)
+{
+	setPosition(xStates[x], yStates[y]);
+}
+
+void CameraGimbal::setX(xPos_t x)
+{
+	m_servoX.Set(xStates[x]);
+}
+
+void CameraGimbal::setY(yPos_t y)
+{
+	m_servoY.Set(yStates[y]);
+}
+/* Not sure which one you wanted
+void CameraGimbal::setX(float xPos)
+{
+	m_servoX.Set(xPos);
+}
+
+void CameraGimbal::setY(float yPos)
+{
+	m_servoY.Set(yPos);
+}
+ */
