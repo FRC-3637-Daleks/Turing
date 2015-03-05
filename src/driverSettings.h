6d87b941aed1154049c09d3a18531ae37dbb3ec9
Elijah Williams <elazertwist@gmail.com>
2015-03-04 20:47:35 -0500

    Adding Driver Settings(rough)
    More updates will be pushed to personal. This was because I need to sync
    files

---
+++ src/driverSettings.h
@@ -0,0 +1,15 @@
+/*
+ * driverSettings.h
+ *
+ *  Created on: Mar 4, 2015
+ *      Author: elija_000
+ */
+
+#ifndef SRC_DRIVERSETTINGS_H_
+#define SRC_DRIVERSETTINGS_H_
+
+
+
+
+
+#endif /* SRC_DRIVERSETTINGS_H_ */
