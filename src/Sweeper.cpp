4048369b1fc7ea125421a55dae46efb63bb1f242
Elijah Williams <elazertwist@gmail.com>
2015-03-09 20:44:58 -0400

    New Sweeper Code

--- src/Sweeper.cpp
+++ src/Sweeper.cpp
@@ -9,35 +9,78 @@
 #include "WPILib.h"
 
 
-Sweeper::Sweeper(uint32_t m_talon)
+// States are in Inches
+const double Sweeper::States[] = {
+		[Sweeper::Down]=0.0,
+		[Sweeper::Intermediate]=5.0,
+		[Sweeper::Up]=10.0
+};
+
+
+Sweeper::Sweeper(uint32_t m_talon, PIDConfig iPID, double ramp)
 {
 	m_t = new CANTalon(m_talon);
-	m_needFree = false;
-	m_t->SetControlMode(CANTalon::ControlMode::kPercentVbus);
+	m_needFree = true;
+	m_t->SetControlMode(CANTalon::ControlMode::kPosition);
+	m_t.SetFeedbackDevice(CANTalon::AnalogPot);
+	m_t.SetPID(iPID.P, iPID.I, iPID.D);
+	m_t.SetIzone(iPID.iZone);
+	m_t.SetCloseLoopRampRate(ramp);
+	m_t.ConfigLimitMode(CANSpeedController::kLimitMode_SwitchInputsOnly);
+	m_t.SetSensorDirection(true);
+
 	return;
 }
 
-Sweeper::Sweeper(CANTalon &m_talon)
+Sweeper::Sweeper(CANTalon &m_talon, PIDConfig iPID, double ramp)
 {
 	m_t = &m_talon;
 	m_needFree = false;
-	m_t->SetControlMode(CANTalon::ControlMode::kPercentVbus);
+	m_t->SetControlMode(CANTalon::ControlMode::kPosition);
+	m_t.SetFeedbackDevice(CANTalon::AnalogPot);
+	m_t.SetPID(iPID.P, iPID.I, iPID.D);
+	m_t.SetIzone(iPID.iZone);
+	m_t.SetCloseLoopRampRate(ramp);
+	m_t.ConfigLimitMode(CANSpeedController::kLimitMode_SwitchInputsOnly);
+	m_t.SetSensorDirection(true);
 	return;
 }
 
-Sweeper::Sweeper(CANTalon *m_talon)
+Sweeper::Sweeper(CANTalon *m_talon, PIDConfig iPID, double ramp)
 {
 	m_t = m_talon;
 	m_needFree = false;
-	m_t->SetControlMode(CANTalon::ControlMode::kPercentVbus);
+	m_t->SetControlMode(CANTalon::ControlMode::kPosition);
+	m_t.SetFeedbackDevice(CANTalon::AnalogPot);
+	m_t.SetPID(iPID.P, iPID.I, iPID.D);
+	m_t.SetIzone(iPID.iZone);
+	m_t.SetCloseLoopRampRate(ramp);
+	m_t.ConfigLimitMode(CANSpeedController::kLimitMode_SwitchInputsOnly);
+	m_t.SetSensorDirection(true);
 	return;
+}
+
+void
+Sweeper::setState(Sweeper::States stateValue)
+{
+	m_t->setState(stateValue);
+return;
+}
+
+void
+Sweeper::setPosition(Sweeper_State_t newPos)
+{
+	m_t->Set(Sweeper::States[newPos]);
+return;
 }
 
 
 void
 Sweeper::setSpeed(float motorSpeed)
 {
+	m_t->SetControlMode(CANTalon::ControlMode::kPercentVbus);
 	m_t->Set(motorSpeed);
+	m_t->SetControlMode(CANTalon::ControlMode::kPosition);
 return;
 }
 
--- src/Sweeper.h
+++ src/Sweeper.h
@@ -10,6 +10,7 @@
 
 #include "WPILib.h"
 #include "Hardware.h"
+#include "Lifter.h"
 
 class Sweeper { //Sweeper no sweeping!
 public:
@@ -17,6 +18,33 @@ public:
 	Sweeper(uint32_t m_talon);
 	Sweeper(CANTalon &m_talon);
 	Sweeper(CANTalon *m_talon);
+
+	struct PIDConfig
+	{
+		double P;
+		double I;
+		double D;
+		unsigned int iZone;
+		PIDConfig(const double iP, const double iI=0.0, const double iD=0.0, const unsigned int iiZone=200): P(iP), I(iI), D(iD), iZone(iiZone) {};
+	};
+
+	enum Sweeper_State_t {Down, Intermediate, Up, NUM_STATES};
+
+
+private:
+	CANTalon m_tal1;
+	PIDConfig pid;
+	double rampRate;
+	double targetPosition;
+	Height_t targetState;
+	bool calibrated;
+
+public:
+	/// Constructor accepts talon ID, PID values, and a Ramp Rate.  and Sweeper is calibrated.
+
+	Sweeper(int talID1, PIDConfig iPID, double ramp);
+	Sweeper(CANTalon *m_talon, PIDConfig iPID, double ramp);
+	Sweeper(CANTalon &m_talon, PIDConfig iPID, double ramp);
 
 	void  setSpeed(float motorSpeed);
 	void  Stop();
